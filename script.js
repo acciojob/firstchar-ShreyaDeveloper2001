2fecc1d2a7e8e3d0f50d8cd8e826ca2945000f7f
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-25 23:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstChar(text) {
   // your code here
+	function firstChar(text) {
+  // Remove leading and trailing spaces using trim()
+  const trimmedText = text.trim();
+
+  // Check if the trimmed text is empty or consists of only spaces
+  if (trimmedText === "") {
+    return "";
+  }
+
+  // Return the first character of the trimmed text
+  return trimmedText[0];
+	
 }
 
 // Do not change the code below
